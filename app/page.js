3bff86d448435a2f128cede7f2a89c9720730007
Rob Hall <r-narch@hotmail.com>
2024-06-04 15:21:52 +0100

    format

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Guess the Country</h1>
+      <p>Welcome to guess the country</p>
+      <button>Play</button>
     </main>
   );
 }
