6123b8ff20890d8a33b29e5475e7e9b539fba3f3
Rob Hall <r-narch@hotmail.com>
2024-06-05 16:25:59 +0100

    if

--- app/ApiButton.js
+++ app/ApiButton.js
@@ -1,15 +1,22 @@
 "use client";
 export default function MyButton() {
+  let country = null;
   const callAPI = async () => {
     try {
       const res = await fetch(
         `https://restcountries.com/v3.1/name/United Kingdom?fullText=true`
       );
       const data = await res.json();
-      console.log(data);
+
+      country = data;
+      console.log(country);
     } catch (err) {
       console.log(err);
     }
   };
+  if (country) {
+    return <p>{country.capital}</p>;
+  }
+
   return <button onClick={callAPI}>Play</button>;
 }
