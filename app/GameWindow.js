6910309657cd5648df7315b1658723c0f019d0d0
Rob Hall <r-narch@hotmail.com>
2024-06-18 15:54:32 +0100

    rows

--- app/GameWindow.js
+++ app/GameWindow.js
@@ -1,1 +1,7 @@
-export default function GameWindow() {}
+export default function GameWindow() {
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div> </div>
+    <div> </div>
+    <div> </div>
+  </main>;
+}
